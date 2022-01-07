df8b59ebc4d54ad5157159701252a3110151f947
Ohlavrac <joaovicrcarvalho@gmail.com>
2022-01-06 23:51:09 -0300

    adicionado consulta de CEP

---
+++ src/controllers/consultaCEP.js
@@ -0,0 +1,46 @@
+let estado = document.getElementsByName("estado");
+let cidade = document.getElementsByName("cidade");
+let index = 0;
+
+const retornadadosCEP = (conteudo) => {
+    if(conteudo.erro) {
+        alert("CEP INVALIDO")
+        for (index = 0; index < estado.length; index++) {
+            estado[index].value=("...");
+            cidade[index].value=("...");   
+        }
+        
+    } else {
+        for (index = 0; index < estado.length; index++) {
+            estado[index].value=(conteudo.uf);
+            cidade[index].value=(conteudo.localidade);
+        }
+    }
+}
+
+const consultarCEP = (cep) => {
+    if (cep != "" || cep != null) {
+        if (cep.length == 8) {
+            for (index = 0; index < estado.length; index++) {
+                estado[index].value=("...");
+                cidade[index].value=("...");   
+            }
+
+            let script = document.createElement("script");
+
+            script.src = "https://viacep.com.br/ws/"+ cep + "/json/?callback=retornadadosCEP";
+
+            document.body.appendChild(script);
+        } else {
+            for (index = 0; index < estado.length; index++) {
+                estado[index].value=("...");
+                cidade[index].value=("...");   
+            } 
+        }
+    } else {
+        for (index = 0; index < estado.length; index++) {
+            estado[index].value=("...");
+            cidade[index].value=("...");   
+        }
+    }
+}
