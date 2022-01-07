7dd49e888ab1e07e7b031436bbf746cb9383a8ac
Ohlavrac <joaovicrcarvalho@gmail.com>
2022-01-07 12:58:09 -0300

    Codigo atualizado

--- src/routes.js
+++ src/routes.js
@@ -2,50 +2,93 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 const criptografia = require("./scripts/criptografia");
+const session = require("express-session");
 
 const { Estabelecimento } = require("./models/Estabelecimento");
 const { RegistraEstabelecimentoNaTabela } = require("./models/Estabelecimento");
+const { Pessoa } = require("./models/Pessoa");
+const { RegistraPessoaNaTabela } = require("./models/Pessoa");
+const validaSintaxeEmail = require("./scripts/validaSintaxeEmail");
 
 /*Responsavel por pegar os dados vindo do client*/
 const bodyParser = require("body-parser");
+const { render } = require("express/lib/response");
 router.use(bodyParser.urlencoded({extended:false}));
 router.use(bodyParser.json());
 
 let tempEmail = "";
 let tempSenha = "";
 
+router.use(session({
+    secret: "wkndawdnwouidnawdnawonds", //é uma chave que cria uma sessão
+    resave: true,
+    saveUninitialized: true,
+}));
+
 //Rotas
 router.get("/", (req, res) => {
     res.render("cadastro.html");
 });
 
-router.get("/registrarEstabelecimento", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views", "registro_estabelecimento.html"));
-});
-
-router.post("/registro", (req, res) => {
+router.post("/", async (req, res) => {
     tempEmail = req.body.email;
     tempSenha = req.body.senha;
-    res.render("maisCadastro.html", {email: tempEmail});
+
+    if (validaSintaxeEmail(tempEmail)) {
+        //verifica se esse email existe dentro da tabela de estabelecimentos
+        const disponibilidadeEmailEstabelecimento = await Estabelecimento.findOne({where: {email: tempEmail}});
+        //verifica se esse email existe dentro da tabela do cliente comuns
+        const disponibilidateEmailCliente = await Pessoa.findOne({where: {email: tempEmail}});
+        
+        if (disponibilidadeEmailEstabelecimento != null || disponibilidateEmailCliente != null) {
+            console.log("Email ja esta em uso ou a senha e muito curta");
+            //res.render("cadastro.html", {error: true});
+        } else {
+            res.render("maisCadastro.html", {email: tempEmail});
+        }
+    } else {
+        console.log("Sintaxe de email invalida");
+        res.render("cadastro.html", {error: true});
+    }
 });
 
 //sistema de login em desenvolvimento
+router.get("/login", (req, res) => {
+    res.render("index.html");
+})
+
 router.post("/login", async (req, res) => {
-    const dadosLogin = await Estabelecimento.findOne({
-        attributes: ['senha'],
+    dadosLogin = await Estabelecimento.findOne({
         where: {
             email: req.body.email
         }
     });
-    console.log(dadosLogin.senha);
+
+    const dadosLoginCliente = await Pessoa.findOne({
+        where: {
+            email: req.body.email
+        }
+    });
+
     if (dadosLogin != null) {
-        var senhaDescriptografada = criptografia.descriptografar(dadosLogin.senha);
+        let senhaDescriptografada = criptografia.descriptografar(dadosLogin.senha);
         if (senhaDescriptografada == req.body.senha) {
-            res.sendFile(path.join(__dirname, "/views", "menuPrincipal.html"));
+            req.session.email = req.body.email;
+            //res.sendFile(path.join(__dirname, "/views", "menuPrincipal.html"));
+            res.render('paginaInicialComercial.html', {dadosLogin: dadosLogin});
+        }
+    } else if(dadosLoginCliente != null) {
+        let senhaDescriptografada2 = criptografia.descriptografar(dadosLoginCliente.senha);
+
+        if (senhaDescriptografada2 == req.body.senha) {
+            req.session.email = req.body.email;
+            //res.sendFile(path.join(__dirname, "/views", "menuPrincipal.html"));
+            infoLogin = dadosLoginCliente;
+            res.render('paginaInicialUsuario.html', {dadosLogin: dadosLoginCliente});
         }
     } else {
         console.log("Senha ou Email incorretos");
-        res.redirect("/");
+        res.redirect("/login");
     }
 });
 
@@ -71,21 +114,44 @@ router.post("/adicionarEstabelecimento", async (req, res) => {
         tipoDeConta: 1
     }
 
-    const disponibilidadeEmail = await Estabelecimento.findOne({where: {email: tempEmail}});
-    if (disponibilidadeEmail === null) {
-        //manda o objeto que foi criado a cima para uma função que vai registrar esses estabelecimento na tabela
-        RegistraEstabelecimentoNaTabela(dadosEstabelecimento);
-        tempEmail = "";
-        tempSenha = "";
-        res.redirect("/");
-    } else {
-        console.log("Email ja esta em uso ou a senha e muito curta");
-        res.redirect("/registro");
-    }
+    //manda o objeto que foi criado a cima para uma função que vai registrar esses estabelecimento na tabela
+    RegistraEstabelecimentoNaTabela(dadosEstabelecimento);
+    tempEmail = "";
+    tempSenha = "";
+    res.redirect("/login");
+
 });
 
 
-router.get("/paginaInicialUsuario", async ), (req, res) =>{
+router.post("/adicionarPessoa", async (req, res) => {
+    //quarda os dados de registro de pessoa em um objeto
+    console.log(tempEmail);
+    dadosPessoa = {
+        nomePessoa: req.body.nomePessoa,
+        sobreNome: req.body.sobreNome,
+        email: tempEmail,
+        senha: tempSenha,
+        urlImagem: req.body.urlImagem,
+        passaPorte: req.body.passaPorte,
+        idadePessoa: req.body.idadePessoa,
+        dataNasc: req.body.dataNasc,
+        cidadePessoa: req.body.cidade,
+        estadoPessoa: req.body.estado,
+        cepPessoa: req.body.cep,
+        tipoDeConta: 0
+    }
+
+    console.log(req.body.cidade);
+    console.log(req.body.estado);
+
+    //manda o objeto que foi criado a cima para uma função que vai registrar esses estabelecimento na tabela
+    RegistraPessoaNaTabela(dadosPessoa);
+    tempEmail = "";
+    tempSenha = "";
+    res.redirect("/login");
+});
+
+router.get("/paginaInicialUsuario"),async (req, res) =>{
     const dadosLogin =  await dadosUser.findOne({
             attributes: ['email'] ['senha'],
             where:{
@@ -94,7 +160,7 @@ router.get("/paginaInicialUsuario", async ), (req, res) =>{
     });
     res.render("paginaInicialUsuario.html", {dadosLogin: dadosLogin})
 
-    }
+}
 router.post("/atualizarPerfilUser",(req,res) => {
     dadoUser.update(
         {
@@ -113,7 +179,5 @@ router.post("/atualizarPerfilUser",(req,res) => {
         }
     )
 });    
- 
 
 module.exports = router;
-
