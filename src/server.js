21790752a51cd210005f7fe3bfacbf2eb76b5522
Your Name <kbb25t@gmail.com>
2022-01-20 11:50:13 -0300

    up

--- src/server.js
+++ src/server.js
@@ -1,40 +1,7 @@
-<<<<<<< HEAD
+
 const express = require("express");
 const server = express();
 const port = 3001;
-const path = require("path");
-const session = require("express-session");
-
-const registroRota = require("./routes/rotaRegistro");
-const pessoaRota = require("./routes/rotaPessoa");
-const estabelecimentoRota = require("./routes/rotaEstabelecimento");
-const loginRota = require("./routes/rotaLogin");
-
-server.use(session({
-    secret: "wkndawdnwouidnawdnawonds", //é uma chave que cria uma sessão
-    resave: true,
-    saveUninitialized: true,
-}));
-
-server.engine('html', require('ejs').renderFile);
-server.set('view engine', 'html');
-server.set('views', path.join(__dirname, '/views'));
-
-//responsavel por entregar os arquivos estaticos
-server.use(express.static(path.join(__dirname, "styles"))); //local dos styles
-server.use(express.static(path.join(__dirname, "images"))); //local das imgs
-server.use(express.static(path.join(__dirname, "controllers"))); //local dos controllers
-server.use(express.static(path.join(__dirname, "models")));
-
-server.use(registroRota);
-server.use(pessoaRota);
-server.use(estabelecimentoRota);
-server.use(loginRota);
-
-=======
-const express = require("express");
-const server = express();
-const port = 3000;
 const path = require("path");
 const session = require("express-session");
 
@@ -68,5 +35,5 @@ server.use(loginRota);
 server.use(eventoRota);
 server.use(agendaRota);
 
->>>>>>> 2c331f566cc4411305b3d1d9353396fea9e97341
+
 server.listen(port, () => console.log("Servidor rodando na porta "+ port));
