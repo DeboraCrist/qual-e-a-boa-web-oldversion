cd32c9e1fb4083162017a5bc7f06d8afba8f45f5
Ohlavrac <joaovicrcarvalho@gmail.com>
2022-03-01 13:22:37 -0300

    Adicionado links para termos de uso e politicas de privacidade

--- src/routes/rotaTermos.js
+++ src/routes/rotaTermos.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 router.get("/termosDeUso", (req, res) => {
     res.render("termosDeUso.html");
+});
+
+router.get("/politicas", (req, res) => {
+    res.render("politicaDePrivacidade.html");
 })
 
 module.exports = router;
